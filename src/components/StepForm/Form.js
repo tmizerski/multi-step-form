ee8b372c330dbc984bfd83690edb4f18ca7d3865
tmizerski <63366996+tmizerski@users.noreply.github.com>
2023-02-06 15:19:24 +0100

    add steps viewer and fix some styles

---
+++ src/components/StepForm/Form.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Form(props) {
+    return (
+        <div className={"form-side-wrapper"}>
+            <h2 className={"form-side-title"}>Personal info</h2>
+            <span className={"form-side-subtitle"}>Please provide your name, email address, and phone number.</span>
+            <form className={"form-side-form"}>
+                <div className={"form-input-container"}>
+                    <label className={"form-label"} htmlFor="name">
+                        Name
+                    </label>
+                    <input className={"form-input"} id={"name"} type="text" placeholder={"e.g John"}/>
+                </div>
+                <div className={"form-input-container"}>
+                    <label className={"form-label"} htmlFor="email">
+                        Email address
+                    </label>
+                    <input className={"form-input"} id="email" type="text" placeholder={"e.g. john@gmail.com"}/>
+                </div>
+                <div className={"form-input-container"}>
+                    <label className={"form-label"} htmlFor="phone">
+                        Phone number
+                    </label>
+                    <input className={"form-input"} id="phone" type="text" placeholder={"e.g 48 555111222"}/>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
--- src/components/StepForm/FormContainer.js
+++ src/components/StepForm/FormContainer.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import "./FormContainer.css";
-import sidebarDesktop from "../../images/bg-sidebar-desktop.svg"
+import Form from "./Form";
 
 function FormContainer(props) {
+
+    const steps = [
+        {
+            number: 1,
+            subtitle: "step 1",
+            title: "your info"
+        }, {
+            number: 2,
+            subtitle: "step 2",
+            title: "select plan"
+        }, {
+            number: 3,
+            subtitle: "step 3",
+            title: "add-ons",
+        }, {
+            number: 4,
+            subtitle: "step 4",
+            title: "summary"
+        }]
     return (
         <div className={"form-container"}>
-            <div className={"side-bar"}></div>
+            <div className={"side-bar-container"}>
+                <div className={"side-bar"}>
+                    {steps && steps.map((step, k) => {
+                        return <div className={"step-number-container"}>
+                            <div className={"step-number"}>{step.number}</div>
+                            <div className={"title-container"}>
+                                <div className={"step-subtitle"}>{step.subtitle.toUpperCase()}</div>
+                                <div className={"step-title"}>{step.title.toUpperCase()}</div>
+                            </div>
+                        </div>
+                    })
+                    }
+                </div>
+            </div>
             <div className={"step-container"}>
-                <form className={"form"}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
-                </form>
+                <Form/>
             </div>
         </div>
     );
